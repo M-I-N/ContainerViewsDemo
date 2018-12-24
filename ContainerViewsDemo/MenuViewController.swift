717b517f810240abfef3cf87b9378c12d2647baf
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2018-12-24 12:23:19 +0600

    Container View Controller - Top/Bottom Bar view show/hide with animation - Completed

---
+++ ContainerViewsDemo/MenuViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MenuViewController.swift
+//  ContainerViewsDemo
+//
+//  Created by Nayem BJIT on 12/24/18.
+//  Copyright © 2018 BJIT Ltd. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "hideControlViews"), object: nil, userInfo: ["hidetop": true, "hidebottom": false])
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ContainerViewsDemo/ViewController.swift
+++ ContainerViewsDemo/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var bottomView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(hideControlViews(_:)), name: Notification.Name(rawValue: "hideControlViews"), object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @objc func hideControlViews(_ notification: Notification){
+        let userInfo = notification.userInfo! as! [String : Bool]
+        if (userInfo["hidetop"]!){
+            UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseOut, animations: {
+                self.topView.isHidden = true
+            }, completion: nil)
+        } else{
+            UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseOut, animations: {
+                self.topView.isHidden = false
+            }, completion: nil)
+        }
+
+        if (userInfo["hidebottom"]!){
+            UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseOut, animations: {
+                self.bottomView.isHidden = true
+            }, completion: nil)
+        } else{
+            UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseOut, animations: {
+                self.bottomView.isHidden = false
+            }, completion: nil)
+        }
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: Notification.Name(rawValue: "hideControlViews"), object: nil)
+    }
 
 }
 
